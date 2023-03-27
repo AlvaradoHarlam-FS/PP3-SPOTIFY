b275c47b6f27c4d9da3b1f6da19f2a7e05d2ed99
AlvaradoHarlam-FS <hjalvarado@student.fullsail.edu>
2023-03-26 20:13:02 -0400

    login  update

--- Spotify-app/src/Login.js
+++ Spotify-app/src/Login.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 
+
+const client_id = process.env.CLIENT_ID
 const AUTH_URL = 
-  `https://accounts.spotify.com/authorize?client_id=25e16af2f17a40cd808ea48ff4f09045&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+  `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
   return (
